d7df589c117fac59a1cc73e16fc4f736ae163a46
TwinkieGorilla <55102976+TwinkieGorilla@users.noreply.github.com>
2019-11-25 20:27:12 -0500

    Add files via upload

--- minegame.js
+++ minegame.js
@@ -1,19 +1,5 @@
-// Firebase API Test
-// Firebase DB GET Request: https://test-9d6bc.firebaseio.com/Easy/First.json
-// https://firestore.googleapis.com/v1/projects/test-9d6bc/databases/(default)/documents/easy
-
-//let docRef = fire
-var request = new XMLHttpRequest();
-request.open("GET", "https://us-central1-test-9d6bc.cloudfunctions.net/helloWorld");
-request.onload = function() {
-    var data = JSON.parse(this.response);
-    if (request.status == 200) {
-        document.getElementById("apiTest").innerHTML = "<h3>" + data.message + "</h3>";
-    } else {
-        document.getElementById("apiTest").innerHTML = "<h3>Connection Error!</h3>";
-    }
-}
-request.send();
+// Disables Right-Click
+window.oncontextmenu = function() {return false;}
 
 // global static vars
 var sEvent;
@@ -100,6 +86,8 @@ function confirm(id) {
                 for (var mineId of minePos) {
                     if (mineId == id) {
                         document.getElementById(mineId).innerHTML = "&#128165;";
+                        alert('Game over!');
+                        start();
                     } else {
                         document.getElementById(mineId).innerHTML = "&#128163;";
                     }
