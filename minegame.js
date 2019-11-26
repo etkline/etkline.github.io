4c6a3c7d91e780c09d0e5791a67f44295155208f
etkline <48033949+etkline@users.noreply.github.com>
2019-11-25 22:15:00 -0500

    Fixed
    A new game should not be started automatically. Also, leave in the REST API call for demonstration purposes (I know it might not work locally but leave it in the GitHub version for now).

--- minegame.js
+++ minegame.js
@@ -1,6 +1,22 @@
 // Disables Right-Click
 window.oncontextmenu = function() {return false;}
 
+// Cloud Firestore GET Request (Don't use during testing):
+// https://firestore.googleapis.com/v1/projects/test-9d6bc/databases/(default)/documents/easy
+
+// Firebase API Test: Send HTTP GET Request to Firebase Cloud Functions
+var request = new XMLHttpRequest();
+request.open("GET", "https://us-central1-test-9d6bc.cloudfunctions.net/helloWorld");
+request.onload = function() {
+    var data = JSON.parse(this.response);
+    if (request.status == 200) {
+        document.getElementById("apiTest").innerHTML = "<h3>" + data.message + "</h3>";
+    } else {
+        document.getElementById("apiTest").innerHTML = "<h3>Connection Error!</h3>";
+    }
+}
+request.send();
+
 // global static vars
 var sEvent;
 var sId;
@@ -83,11 +99,11 @@ function confirm(id) {
             }
 
             if (minePos.includes(id)) {
+                alert('Game over!');
+
                 for (var mineId of minePos) {
                     if (mineId == id) {
                         document.getElementById(mineId).innerHTML = "&#128165;";
-                        alert('Game over!');
-                        start();
                     } else {
                         document.getElementById(mineId).innerHTML = "&#128163;";
                     }
