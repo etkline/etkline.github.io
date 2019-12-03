038dfccb7f323de6a62752a0dc9581ef4042c82d
etkline <48033949+etkline@users.noreply.github.com>
2019-12-02 23:04:04 -0500

    Get Requests
    The GET Requests for the REST API are complete. Currently disabled to avoid exceeding read quota (shouldn't happen, but playing it safe).

--- scores.js
+++ scores.js
@@ -4,15 +4,11 @@
 // Firebase API Test: Send HTTP GET Request to Firebase Cloud Functions
 function viewScores() {
     var request = new XMLHttpRequest();
-    request.open("GET", "https://us-central1-test-9d6bc.cloudfunctions.net/helloWorld");
+    request.open("GET", "https://us-central1-test-9d6bc.cloudfunctions.net/easyScores");
 
     request.onload = function() {
         var data = JSON.parse(this.response);
-        if (request.status == 200) {
-            document.getElementById("apiTest").innerHTML = "<h3>" + data.message + "</h3>";
-        } else {
-            document.getElementById("apiTest").innerHTML = "<h3>Connection Error!</h3>";
-        }
+        console.log(data);
     }
     //request.send(); // Comment out line when wanting to disable http requests
 }
