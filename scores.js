67ceda14c3eefec5d7278327fc0de1f7b5c20eb3
etkline <48033949+etkline@users.noreply.github.com>
2019-12-03 11:23:52 -0500

    Firestore API
    Read and Create Requests for documents are now complete.

--- scores.js
+++ scores.js
@@ -12,3 +12,13 @@ function viewScores() {
     }
     //request.send(); // Comment out line when wanting to disable http requests
 }
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 201) {
+        console.log(JSON.parse(this.response));
+    }
+};
+xhttp.open("POST", "https://us-central1-test-9d6bc.cloudfunctions.net/insertScore");
+xhttp.setRequestHeader("Content-Type", "application/json");
+//xhttp.send('{"difficulty":"hard","name":"test","time":518}'); // Comment out line when wanting to disable http requests
